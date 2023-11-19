2889f8991ff5a4f436c4e9e5c308ede7c835af0e
0xRamadan <mah64725@gmail.com>
2023-11-19 18:08:16 +0200

    fix bug in form builder component

--- components/FormBuilder.tsx
+++ components/FormBuilder.tsx
@@ -64,8 +64,62 @@ const FormBuilder = ({ form }: { form: Form }) => {
     );
   }
 
+  const shareUrl = `${window.location.origin}/submit/${form.shareURL}`;
+
   if (form.published) {
-    <FormPublished form={form} />;
+    
+    // try to make it a separate component, notice that you use useRouter which is only work on server component
+    // so try make it a server component.
+    return (
+      <>
+        <Confetti
+          width={window.innerWidth}
+          height={window.innerHeight}
+          recycle={false}
+          numberOfPieces={1000}
+        />
+        <div className="flex h-full w-full flex-col items-center justify-center">
+          <div className="max-w-md">
+            <h1 className="mb-10 border-b pb-2 text-center text-4xl font-bold text-primary">
+              🎊🎊 Form Published 🎊🎊
+            </h1>
+            <h2 className="text-2xl">Share this form</h2>
+            <h3 className="border-b pb-10 text-xl text-muted-foreground">
+              Anyone with the link can view and submit the form
+            </h3>
+            <div className="my-4 flex w-full flex-col items-center gap-2 border-b pb-4">
+              <Input className="w-full" readOnly value={shareUrl} />
+              <Button
+                className="mt-2 w-full"
+                onClick={() => {
+                  navigator.clipboard.writeText(shareUrl);
+                  toast({
+                    title: "Copied!",
+                    description: "Link copied to clipboard",
+                  });
+                }}
+              >
+                Copy link
+              </Button>
+            </div>
+            <div className="flex justify-between">
+              <Button variant={"link"} asChild>
+                <Link href={"/"} className="gap-2">
+                  <BsArrowLeft />
+                  Go back home
+                </Link>
+              </Button>
+              <Button variant={"link"} asChild>
+                <Link href={`/forms/${form.id}`} className="gap-2">
+                  Form details
+                  <BsArrowRight />
+                </Link>
+              </Button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
   }
 
   return (
@@ -88,60 +142,6 @@ const FormBuilder = ({ form }: { form: Form }) => {
       </main>
       <DragOverlayWrapper />
     </DndContext>
-  );
-};
-
-const FormPublished = ({ form }: { form: Form }) => {
-  const shareUrl = `${window.location.origin}/submit/${form.shareURL}`;
-  return (
-    <>
-      <Confetti
-        width={window.innerWidth}
-        height={window.innerHeight}
-        recycle={false}
-        numberOfPieces={1000}
-      />
-      <div className="flex h-full w-full flex-col items-center justify-center">
-        <div className="max-w-md">
-          <h1 className="mb-10 border-b pb-2 text-center text-4xl font-bold text-primary">
-            🎊🎊 Form Published 🎊🎊
-          </h1>
-          <h2 className="text-2xl">Share this form</h2>
-          <h3 className="border-b pb-10 text-xl text-muted-foreground">
-            Anyone with the link can view and submit the form
-          </h3>
-          <div className="my-4 flex w-full flex-col items-center gap-2 border-b pb-4">
-            <Input className="w-full" readOnly value={shareUrl} />
-            <Button
-              className="mt-2 w-full"
-              onClick={() => {
-                navigator.clipboard.writeText(shareUrl);
-                toast({
-                  title: "Copied!",
-                  description: "Link copied to clipboard",
-                });
-              }}
-            >
-              Copy link
-            </Button>
-          </div>
-          <div className="flex justify-between">
-            <Button variant={"link"} asChild>
-              <Link href={"/"} className="gap-2">
-                <BsArrowLeft />
-                Go back home
-              </Link>
-            </Button>
-            <Button variant={"link"} asChild>
-              <Link href={`/forms/${form.id}`} className="gap-2">
-                Form details
-                <BsArrowRight />
-              </Link>
-            </Button>
-          </div>
-        </div>
-      </div>
-    </>
   );
 };
 
