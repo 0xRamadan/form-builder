529341c068b2ecbea94125cdd52be6a54d2f8f69
0xRamadan <mah64725@gmail.com>
2023-12-09 14:12:02 +0200

    finish form submission page functionality

--- app/(dashboard)/forms/[id]/page.tsx
+++ app/(dashboard)/forms/[id]/page.tsx
@@ -1,12 +1,15 @@
-import { GetFormById } from "@/actions/forms";
+import { GetFormById, GetFormWithSubmissions } from "@/actions/forms";
 import FormLinkShare from "@/components/FormLinkShare";
 import VisitBtn from "@/components/VisitBtn";
-import React from "react";
+import React, { ReactNode } from "react";
 import { StatsCard } from "../../page";
 import { LuView } from "react-icons/lu";
 import { FaWpforms } from "react-icons/fa";
 import { HiCursorClick } from "react-icons/hi";
 import { TbArrowBounce } from "react-icons/tb";
+import { ElementsType, FormElementInstance } from "@/components/FormElement";
+import { Table, TableHeader, TableRow, TableHead, TableBody, TableCell } from "@/components/ui/table";
+import { formatDistance } from "date-fns";
 
 const FormDetailPage = async ({ params }: { params: { id: string } }) => {
   const { id } = params;
@@ -30,12 +33,12 @@ const FormDetailPage = async ({ params }: { params: { id: string } }) => {
           <VisitBtn shareUrl={form.shareURL} />
         </div>
       </div>
-      <div className="py-4 border-b border-muted">
-        <div className="container flex gap-2 items-center justify-between">
+      <div className="border-b border-muted py-4">
+        <div className="container flex items-center justify-between gap-2">
           <FormLinkShare shareUrl={form.shareURL} />
         </div>
       </div>
-      <div className="w-full pt-8 gap-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 container">
+      <div className="container grid w-full grid-cols-1 gap-4 pt-8 md:grid-cols-2 lg:grid-cols-4">
         <StatsCard
           title="Total visits"
           icon={<LuView className="text-blue-600" />}
@@ -82,9 +85,93 @@ const FormDetailPage = async ({ params }: { params: { id: string } }) => {
 
 export default FormDetailPage;
 
+type Column = {
+  id: string;
+  label: string;
+  required: boolean;
+  type: ElementsType;
+};
 
-function SubmissionsTable({id}:{id: number}) {
+type Row = { [key: string]: string } & {
+  submittedAt: Date;
+};
+
+
+async function SubmissionsTable({ id }: { id: number }) {
+  const form = await GetFormWithSubmissions(id);
+
+  if (!form) {
+    throw new Error("form not found");
+  }
+
+  const formElements = JSON.parse(form.content) as FormElementInstance[];
+
+  const columns: Column[] = [];
+  formElements.forEach((element) => {
+    switch (element.type) {
+      case "TextField":
+        columns.push({
+          id: element.id,
+          label: element.extraAttributes?.label,
+          required: element.extraAttributes?.required,
+          type: element.type,
+        });
+        break;
+      default:
+        break;
+    }
+  });
+
+
+  const rows: Row[] = [];
+  form.FormSubmissions.forEach((submission) => {
+    const content = JSON.parse(submission.content);
+    rows.push({
+      ...content,
+      submittedAt: submission.createdAt,
+    });
+  });
+
+
+
   return (
-    <div>submissions</div>
-  )
+    <>
+      <h1 className="text-2xl font-bold my-4">Submissions</h1>
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              {columns.map((column) => (
+                <TableHead key={column.id} className="uppercase">
+                  {column.label}
+                </TableHead>
+              ))}
+              <TableHead className="text-muted-foreground text-right uppercase">Submitted at</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {rows.map((row, index) => (
+              <TableRow key={index}>
+                {columns.map((column) => (
+                  <RowCell key={column.id} type={column.type} value={row[column.id]} />
+                ))}
+                <TableCell className="text-muted-foreground text-right">
+                  {formatDistance(row.submittedAt, new Date(), {
+                    addSuffix: true,
+                  })}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </>
+  );;
+}
+
+
+function RowCell({ type, value }: { type: ElementsType; value: string }) {
+  let node: ReactNode = value;
+
+  return <TableCell>{node}</TableCell>;
 }
